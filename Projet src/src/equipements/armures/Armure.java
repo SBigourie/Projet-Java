2615875cfcdd87f222369cfce2d86f168860d970
Elie Jourdan <elie31620@hotmail.fr>
2022-04-12 15:33:45 +0200

    Class armure

--- Projet src/src/equipements/armures/Armure.java
+++ Projet src/src/equipements/armures/Armure.java
@@ -1,4 +1,40 @@
 package equipements.armures;
 
+import protagonistes.humain.Humain;
+
 public class Armure {
+	
+	private Humain proprietaire;
+    private String nature;
+    private int nbVieAjouter;
+    
+    public Armure(String nature, int nbVieAjouter) {
+    	this.nature = nature;
+    	this.nbVieAjouter = nbVieAjouter;
+    }
+
+	public Humain getProprietaire() {
+		return proprietaire;
+	}
+
+	public void setProprietaire(Humain proprietaire) {
+		this.proprietaire = proprietaire;
+	}
+
+	public String getNature() {
+		return nature;
+	}
+
+	public void setNature(String nature) {
+		this.nature = nature;
+	}
+
+	public int getNbVieAjouter() {
+		return nbVieAjouter;
+	}
+
+	public void setNbVieAjouter(int nbVieAjouter) {
+		this.nbVieAjouter = nbVieAjouter;
+	}
+    
 }
