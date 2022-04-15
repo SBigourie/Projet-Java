1b2874cbfffceae67c3056925c96ea1676f5cef9
Samuel Bigourie <sbigourie@gmail.com>
2022-04-15 15:17:34 +0200

    tableau

--- Projet src/src/Echiquier/Echiquier.java
+++ Projet src/src/Echiquier/Echiquier.java
@@ -4,15 +4,41 @@ import protagonistes.EtreVivant;
 
 public class Echiquier {
 
-//plateau
-    private EtreVivant[][] plateau = new EtreVivant[10][10];
+    int nbEtreVivant = 10;
+    private EtreVivant[][] plateau = new EtreVivant[nbEtreVivant][nbEtreVivant];
 
-    //tour
-//    private char tour = "c";
-
+//nombre de case
 
     public Echiquier(){
-        this.plateau[0][0] = new EtreVivant("prinplouf",150);
+        for (int i = 0; i < nbEtreVivant; i++) {
+            for (int j = -1; j < 5; j++) {
+                plateau[0][j+1] = new EtreVivant("toto",150);
+            }
+            for (int j = -1; j < 5; j++) {
+                plateau[1][j+1] = new EtreVivant("tata", 200);
+            }
+        }
+    }
+    public void affichage(){
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        for (int i = 0; i < nbEtreVivant; i++) {
+            for (int j = 0; j < nbEtreVivant/2; j++) {
+                if (plateau[i][j] != null) {
+                    System.out.print("| " + plateau[i][j].toString() + " ");
+                }
+                else {
 
+                    System.out.print("|                           ");
+                }
+            }
+            System.out.println("|");
+            System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+        }
+
+    }
+    public static void main (String[] args){
+        Echiquier echiquier = new Echiquier();
+        echiquier.affichage();
     }
 }
